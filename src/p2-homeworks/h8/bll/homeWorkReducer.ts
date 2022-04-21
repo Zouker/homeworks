426ef61a87f575213ec980e9cb5de2fce6b85759
Denis Shubin <denis.shubin@gmail.com>
2022-04-21 16:50:33 +0300

    HW8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,27 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {initialPeople, UserType} from '../HW8';
+
+export type ActionType = {
+    type: string,
+    payload: number | string
+}
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
+            if (action.payload === 'up') {
+                let state = [...initialPeople.sort((a, b) => a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1)]
+                return state
+            } else if (action.payload === 'down') {
+                let state = [...initialPeople.sort((a, b) => a.name.toLowerCase() > b.name.toLowerCase() ? -1 : 1)]
+                return state
+            }
             return state
         }
         case 'check': {
-            // need to fix
+            let state = initialPeople.filter(a => a.age > action.payload)
             return state
         }
-        default: return state
+        default:
+            return state
     }
 }
